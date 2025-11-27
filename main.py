738934a605eeb788f30d8de521a64d492b0ce6f1
Arthur-Dent548 <kchard7@gmail.com>
2025-11-27 21:32:38 +0000

    CH6 L6 complete - project finished

--- main.py
+++ main.py
@@ -37,6 +37,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
         for drawable_object in drawable:
             drawable_object.draw(screen)
         pygame.display.flip()
